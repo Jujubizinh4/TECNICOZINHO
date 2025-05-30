cac700c8f87f8e9921f3bfac1fda969126067e80
Borges Julia <juliahelenaborges820@gmail.com>
2025-05-30 08:21:03 -0300

    Add files via upload

---
+++ LendoTextinho.py
@@ -0,0 +1,7 @@
+with open('textinho.txt','r') as arquivo:
+#a gente leu o arquivo porem não fizemos nada com ele ;3 por isso não rolo nada
+    conteudo = arquivo.read()
+    linhas = arquivo.readlines()
+    for linha in linhas:
+        pass
+    
