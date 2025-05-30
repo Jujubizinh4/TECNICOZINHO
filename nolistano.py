257f6503321b7dc40e44b68e3874c40b0505cdf2
Borges <juliahelenaborges820@gmail.com>
2025-05-30 07:45:12 -0300

    apenas mais um teste (se por um numero>= 20 vai fala: boa,caso contrario:se deu mal)

---
+++ nolistano.py
@@ -0,0 +1,8 @@
+diguidin = float(input("Digite um valor"))
+
+if diguidin >= 20:
+    resultado = "Boa campeão"
+    print(resultado)
+else:
+    resultado = "Se deu mal campeão"
+    print(resultado)
