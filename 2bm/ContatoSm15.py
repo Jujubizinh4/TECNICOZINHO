6bc581a6b0938ca6b42bba5bf35ef946fbb07cbb
Borges <juliahelenaborges820@gmail.com>
2025-08-05 10:30:34 -0300

    correção de codigos

---
+++ 2bm/ContatoSm15.py
@@ -0,0 +1,11 @@
+contatos = []
+
+opcao = 1
+i = 1
+
+while opcao != 0:
+    opcao = int(input())
+    contatos[i] = int(input())
+    i = i + i
+    print(contatos)
+#terminar dps com o rossi
---
+++ 2bm/PACD_Sm14.py
@@ -0,0 +1,18 @@
+aluno1=[1,7.5]
+aluno2=[2,9.6]
+aluno3=[3,5.5]
+
+if aluno1[1] >= 7:
+    print("nota boa")
+else:
+    print("estude!")
+    
+if aluno2[1] >= 7:
+    print("nota boa")
+else:
+    print("estude!")
+    
+if aluno3[1] >= 7:
+    print("nota boa")
+else:
+    print("estude!")
