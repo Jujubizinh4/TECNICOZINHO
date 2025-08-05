466457a7a86b963200c03a26f1ceec82f9b14290
Borges <juliahelenaborges820@gmail.com>
2025-08-05 09:13:43 -0300

    este código foi feito em sala de aula porém nao esta funcionando corretamente(esqueci de termina e vou envia assim mesmo)

--- Programação em grupo.py
+++
@@ -1,9 +0,0 @@
-#nesta aula a gente escreveu o codigo na lousa e tivemos que passar para o vsCode
-lais=float(input())
-lapis=float(input())
-lacerda=(lais+lapis)/2
-if lacerda >=8:
-    print("excelente")
-elif lacerda >=5:
-    else
-    print("insatisfeito")
