0377424711f8d18de01b9d641675670ff31aad84
Borges <juliahelenaborges820@gmail.com>
2025-09-02 10:17:03 -0300

    um pouco de matriz, alguns testes ;3

---
+++ 3bm/LAOPE22.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a= np.array ([[4,5],[7,2],[9,3]])
+b= np.array ([[3,2],[5,3]])
+c= a@b
+print(c)
---
+++ 3bm/import.py
@@ -0,0 +1,5 @@
+import numpy as np
+a = np.array ([[2,3,5], [4,7,8]])
+b = np.array ([[2,2,1], [(-2),(-2),1]])
+c = a+b 
+print(c)
